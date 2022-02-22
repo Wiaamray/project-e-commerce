047ea755ef5b76e2c7ee6d40e57fc63743c40ba8
Wiaam <wihemray@mail.com>
2022-02-22 08:37:53 +0100

    Ajout Header/footer/style

--- Wiaam RHAYA/inc/footer.php
+++ Wiaam RHAYA/inc/footer.php
@@ -1,8 +1,67 @@
 
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
 
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+
+<!--Début footer-->
+
+<footer>
+
+<div class="contenu-footer">
+
+<div class="bloc footer-services">
+    <h3>Services</h3>
+    <ul class="liste-services">
+        <li><a href="#"></a>Livraison Rapide</li>
+        <li><a href="#"></a>Rapidité</li>
+        <li><a href="#"></a>Facilité de commander</li>
+        <li><a href="#"></a></li>
+</ul>
+</div>
+
+<div class="bloc footer-contact">
+    <h3>Contact</h3>
+    <p>55-55-55-55</p>
+    <p>DeliveryFood@gmail.com</p>
+    <p>6 rue de l'invention, Paris, 75007</p>
+    
+  
+</div>
+
+
+<div class="bloc footer-horraire">
+    <h3> Horraires</h3>
+    <ul class="liste-horraire">
+        <li>Lund 10h-19h</li>
+        <li>Mar 10h-19h</li>
+        <li>Mer 10h-19h</li>
+        <li>Jeu 10h-19h</li>
+        <li>ven 10h-19h</li>
+</ul>
+</div>
+</div>
+<div class="bloc footer-copyright">
+<p>&copy; 2022 Droits réservés  DeliveryFood</p>
+</div>
+</footer>
+
+<!--Fin footer-->
+
+
 
 </body>
 </html>
--- Wiaam RHAYA/inc/header.php
+++ Wiaam RHAYA/inc/header.php
@@ -17,11 +17,10 @@
 
 ?>
 
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
     <div class="container-fluid">
-        <a class="navbar-brand" href="">Delivery Food
-        
-        </a>
+        <a class="navbar-brand" href="<?= SITE; ?>">DeliveryFood</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03"
                 aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -30,65 +29,245 @@
         <div class="collapse navbar-collapse" id="navbarColor03">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link active" href="">Accueil
+                    <a class="nav-link active" href="<?= SITE; ?>">Accueil
 
                     </a>
                 </li>
+                <?php if (admin()):
 
+                    ?>
                     <li class="nav-item">
-                        <a class="nav-link" href=""></a>
+                        <a class="nav-link" href="<?= SITE . 'admin/ajoutProduit.php'; ?>">Ajout produit</a>
                     </li>
-
+                <?php endif; ?>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Promotions</a>
+                    <a class="nav-link" href="#">A propos</a>
                 </li>
 
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
-                       aria-haspopup="true" aria-expanded="false">Toutes les offres </a>
+                       aria-haspopup="true" aria-expanded="false">Toutes les offres</a>
                     <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Promotions</a>
                         <a class="dropdown-item" href="#">Another action</a>
                         <a class="dropdown-item" href="#">Something else here</a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Contact</a>
+                        <a class="dropdown-item" href="#">Separated link</a>
                     </div>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" href="">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="<?= SITE . 'front/fullCart.php'; ?>">
                         <button type="button" class="rounded btn btn-outline-warning position-relative p-2 ">
                             <i class="fa-solid fa-cart-arrow-down fa-2xl "></i>
                             <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-info">
-
+    <?=  getQuantity() ; ?>+
 
   </span>
                         </button>
                     </a>
                 </li>
             </ul>
-            <form class="d-flex">
-                <input class="form-control me-sm-2" type="text" placeholder="Search">
-                <button class="btn btn-secondary my-2 my-sm-0" type="submit">Rechercher</button>
-            </form>
-
-                <div class="text-center ">
-                    <a href="<?= SITE . 'security/login.php'; ?>" class="btn btn-success">Se connecter</a>
+            
+            <?php if (!connect()):
+                ?>
+                <div class="text-center flex ">
+                    <a href="<?= SITE . 'security/login.php'; ?>" class="btn btn-secondary">Se connecter</a>
                     <a href="<?= SITE . 'security/register.php'; ?>" class="btn btn-primary ">S'inscrire</a>
                 </div>
+           
 
+     <!--Main Navigation-->
+     <header>
+
+
+            <!-- Links -->
+
+            <!-- Social Icon  -->
+            <ul class="navbar-nav nav-flex-icons">
+                <li class="nav-item">
+                    <a class="nav-link"><i class="fab fa-facebook fa-2x"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link"><i class="fab fa-twitter fa-2x"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link"><i class="fab fa-instagram fa-2x"></i></a>
+                </li>
+            </ul>
+        </div>
+   
+    </div>
+
+</nav>
+
+
+<div id="header" class="view">
+                </div>
+
+
+
+</header>
+
+<!-- Fin de header-->
+
+
+ <!--A propos section-->
+
+ <main class="mt-5">
+<div class="container">
+
+    
+    <section id="best-features" class="text-center">
+
+        
+        <h2 class="mb-5 font-weight-bold">DeliveryFood</h2>
+
+        <!--Grid row-->
+        <div class="row d-flex justify-content-center mb-4">
+
+            <!--Grid column-->
+            <div class="col-md-8">
+
+                <!-- Description -->
+                <p class="grey-text">
+                            Notre compagne à pour objectif <br/>la livraison à domicile des repas frais et  équilibrés.
+                             Est parfaite pour ceux qui fuient les produits industriels.<br/>
+                             On prend on considération la tendance écologique afin d’éviter le gaspillage alimentaire et protéger la planéte. </p>
+
+            </div>
+
+                         <!--A propos section Fin-->
+
+
+<!--Section Card-->
+
+<div class="row justify-content-around">
+    <H1> Les Menus </H1>
+
+    <div class="card my-2" style="width: 18rem;">
+        <img src="upload/M3.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Toaste Avocat</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> <div class="star mt-3 align-items-center">
+            <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></div>
+          <div class="cost mt-3 text-dark">
+            <span>16€ </span>
+      
+    </div>
+          <a href="#" class="btn btn-primary">Ajouter à la carte</a>
+        </div>
+        
+      </div>
+
+      <div class="card my-2" style="width: 18rem;">
+        <img src="upload/M4.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Aubergine Farcie</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p><div class="star mt-3 align-items-center">
+            <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></div>
+          <div class="cost mt-3 text-dark">
+            <span>13 € </span>
+          
+    </div>
+          <a href="#" class="btn btn-primary">Ajouter à la carte</a>
+        </div>
+      </div>
+
+      <div class="card my-2" style="width: 18rem;">
+        <img src="upload/M5.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Bowl Salade</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p><div class="star mt-3 align-items-center">
+            <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></div>
+          <div class="cost mt-3 text-dark">
+            <span>16 € </span>
+         
+          
+        
+    </div>
+          <a href="#" class="btn btn-primary">Ajouter à la carte</a>
+        </div>
+      </div>
+
+      <div class="card my-2" style="width: 18rem;">
+        <img src=upload/M2.png class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Bowl Légumes</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p><div class="star mt-3 align-items-center">
+            <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></div>
+          <div class="cost mt-3 text-dark">
+            <span>16 € </span>
+         
+          
+        
+    </div>
+          <a href="#" class="btn btn-primary">Ajouter à la carte</a>
+        </div>
+      </div>
+
+      <div class="card my-2" style="width: 18rem;">
+        <img src="upload/M3.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Toaste Avocat</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p><div class="star mt-3 align-items-center">
+            <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></div>
+          <div class="cost mt-3 text-dark">
+            <span>16 € </span>
+         
+          
+        
+    </div>
+          <a href="#" class="btn btn-primary">Ajouter à la carte</a>
+        </div>
+      </div>
+
+
+      <div class="card my-2" style="width: 18rem;">
+        <img src="upload/M6.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Salade Fraiche</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p><div class="star mt-3 align-items-center">
+            <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></div>
+          <div class="cost mt-3 text-dark">
+            <span>16 € </span>
+         
+          
+        
+    </div>
+          <a href="#" class="btn btn-primary">Ajouter à la carte</a>
+        </div>
+      </div>
+
+
+
+     
+
+
+
+
+  
+
+  
+
+   
+
+</div>
+
+<!--Fin section card-->
+
+
+<?php else: ?>
                 <div class="text-center ">
                     <a href="<?= SITE . '?unset=1'; ?>" class="btn btn-primary mt-1"><i
                                 class="fa-solid fa-power-off"></i></a>
                 </div>
-
+            <?php endif; ?>
         </div>
     </div>
 </nav>
-
-<div class="header">
-    <img width:100%; src="Wiaam RHAYA/upload/img-header">
-</div>
-
 <div class="container">
     <?php if (isset($_SESSION['messages']) && !empty($_SESSION['messages'])):
         foreach ($_SESSION['messages'] as $type => $mess):
@@ -100,5 +279,6 @@
                 </div>
                 <?php unset($_SESSION['messages'][$type][$key]); ?>
             <?php endforeach; endforeach; endif; ?>
+
 
 
