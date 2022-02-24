807b3bc7da825bce553215e7c704e309dd782747
Matt <mprigentsuj@gmail.com>
2022-02-24 16:42:28 +0100

    Commit Matthieu mise en place V1

---
+++ Matthieu/contact.php
@@ -0,0 +1,65 @@
+
+<?php
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
+
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+
+
+
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+
+
+
+//Create an instance; passing `true` enables exceptions
+
+
+
+$mail = new PHPMailer(true);
+
+
+    //Server settings
+                     //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'dorancosalle78@gmail.com';                     //SMTP username
+    $mail->Password   = 'Dorancosalle78connect';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465; 
+
+
+
+
+    $nom = $_POST["nom"];
+    $email = $_POST["email"];
+    $sujet = $_POST["sujet"];
+    $message = $_POST["message"];
+   
+    //Destinataires
+    $mail->addAddress("wihemray@gmail.com");
+    //Expéditeur//
+    $mail->setFrom($email);
+
+    $mail->addEmbeddedImage('upload/photo.jpg', 'logo');
+
+    $mail->isHTML(true);
+    //contenu
+    $mail->Subject = $sujet; 
+
+
+
+    ob_start();
+    include('mail.php');
+    $mail->Body = ob_get_clean();  
+    // On envoie
+      $mail->send();
+      header('location:./');
+      exit();
+  
